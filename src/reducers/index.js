7ec97c4dddaef828a66ab15d710a71f3595bba78
xd8c <73834700+SauravHi@users.noreply.github.com>
2023-11-16 08:58:28 +0000

    Updated Changes

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import changeNumber from "./updown";
-
-const rootReducer = combineReducers({
-  changeNumber: changeNumber
-});
-
-export default rootReducer;
